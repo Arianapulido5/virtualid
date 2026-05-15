25922194be1254217595c0ddb7a963e231db4de8
Arianapulido5 <20223l001044@utcv.edu.mx>
2026-05-15 12:36:23 -0600

    update production environment urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.0.6:3000/api',
-  qrValidarUrl: 'https://frumentaceous-overnervously-johnnie.ngrok-free.dev/api/qr/validar'
+  apiUrl: 'https://virtualid-api-production.up.railway.app/api',
+  qrValidarUrl: 'https://virtualid-api-production.up.railway.app/api/qr/validar'
 };
